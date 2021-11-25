183ade27d90086e94422fd89880fdc8ec4cc4ca4
YannickSchutzbach <yannick.schutzbach@mni.thm.de>
2021-11-25 03:48:33 +0100

    HÜ1 Funktionen noch implementieren

--- Hausuebung1/script/script.js
+++ Hausuebung1/script/script.js
@@ -1,16 +1,32 @@
-function test1(event) {
+function swapCurrency(event) {
     event.preventDefault();
-    console.log('1');
+    var newRate = 1 / (Number(document.getElementById('exchangeRate').value));
+    if (!isFinite(newRate)) {
+        alert("Exchange rate is no number");
+        return;
+    }
+    var currency1 = document.getElementById('curr1').value;
+    var currency2 = document.getElementById('curr2').value;
+    document.getElementById('exchangeRate').value = String(newRate);
+    document.getElementById('curr1').value = currency2;
+    document.getElementById('curr2').value = currency1;
 }
-function test2(event) {
+function calculateTable(event) {
     event.preventDefault();
-    console.log('2');
+    var newRate = 1 / (Number(document.getElementById('exchangeRate').value));
+    if (!isFinite(newRate)) {
+        alert("Exchange rate is no number");
+        return;
+    }
+    var currency1 = document.getElementById('curr1').value;
+    var currency2 = document.getElementById('curr2').value;
+    document.getElementById('tableTitle').value = "Exchange rate " + currency2 + "/" + currency1 + " : " + newRate;
 }
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.getElementById('change').addEventListener("submit", function (event) {
-        test1(event);
+    document.getElementById('swapButton').addEventListener("click", function (event) {
+        swapCurrency(event);
     });
-    document.getElementById('calc').addEventListener("submit", function (event) {
-        test2(event);
+    document.getElementById('calcButton').addEventListener("click", function (event) {
+        calculateTable(event);
     });
 });
--- Hausuebung1/script/script.ts
+++ Hausuebung1/script/script.ts
@@ -1,18 +1,39 @@
-function test1(event) {
+function swapCurrency(event) {
     event.preventDefault();
-    console.log('1');
+
+    let newRate = 1/(Number((<HTMLInputElement>document.getElementById('exchangeRate')).value));
+    if (!isFinite(newRate)){
+        alert("Exchange rate is no number");
+        return;
+    }
+    let currency1 = (<HTMLInputElement>document.getElementById('curr1')).value;
+    let currency2 = (<HTMLInputElement>document.getElementById('curr2')).value;
+
+    (<HTMLInputElement>document.getElementById('exchangeRate')).value = String(newRate);
+    (<HTMLInputElement>document.getElementById('curr1')).value = currency2;
+    (<HTMLInputElement>document.getElementById('curr2')).value = currency1;
 }
-function test2(event) {
+
+function calculateTable(event) {
     event.preventDefault();
-    console.log('2');
+
+    let newRate = 1/(Number((<HTMLInputElement>document.getElementById('exchangeRate')).value));
+    if (!isFinite(newRate)){
+        alert("Exchange rate is no number");
+        return;
+    }
+    let currency1 = (<HTMLInputElement>document.getElementById('curr1')).value;
+    let currency2 = (<HTMLInputElement>document.getElementById('curr2')).value;
+
+    (<HTMLInputElement>document.getElementById('tableTitle')).value = "Exchange rate "+currency2+"/"+currency1+" : " + newRate;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('change').addEventListener("submit", (event) => {
-        test1(event);
+    document.getElementById('swapButton').addEventListener("click", (event) => {
+        swapCurrency(event);
     })
-    document.getElementById('calc').addEventListener("submit", event =>{
-        test2(event);
+    document.getElementById('calcButton').addEventListener("click", (event) => {
+        calculateTable(event);
     })
-})
 
+})
