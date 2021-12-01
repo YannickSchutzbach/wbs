753226e7fa3d75e7635b6f2060f13204cd6ec834
YannickSchutzbach <yannick.schutzbach@mni.thm.de>
2021-12-01 11:26:03 +0100

    HÜ1 fertig + optional

--- Hausuebung1/script/script.ts
+++ Hausuebung1/script/script.ts
@@ -16,6 +16,7 @@ function swapCurrency(event) {
     (<HTMLInputElement>document.getElementById('exchangeRate')).value = String(newRate.toFixed(2));
     (<HTMLInputElement>document.getElementById('curr1')).value = currency2;
     (<HTMLInputElement>document.getElementById('curr2')).value = currency1;
+
     calculateTable(event);
 }
 
@@ -34,6 +35,128 @@ function calculateTable(event) {
         return;
     }
 
+    //generate Table
+    document.getElementById('table').innerHTML=`
+    <table class="table">
+    <thead>
+    <td id="tableCurrency1"></td>
+    <td id="tableCurrency2"></td>
+    </thead>
+    <tr class="1">
+        <td id="10">1.00</td>
+        <td id="11"></td>
+    </tr>
+    <tr class="1">
+        <td id="20">2.00</td>
+        <td id="21"></td>
+    </tr>
+    <tr class="1">
+        <td id="30">3.00</td>
+        <td id="31"></td>
+    </tr>
+    <tr class="1">
+        <td id="40">4.00</td>
+        <td id="41"></td>
+    </tr>
+    <tr class="1">
+        <td id="50">5.00</td>
+        <td id="51"></td>
+    </tr>
+    <tr class="1">
+        <td id="60">6.00</td>
+        <td id="61"></td>
+    </tr>
+    <tr class="1">
+        <td id="70">7.00</td>
+        <td id="71"></td>
+    </tr>
+    <tr class="1">
+        <td id="80">8.00</td>
+        <td id="81"></td>
+    </tr>
+    <tr class="1">
+        <td id="90">9.00</td>
+        <td id="91"></td>
+    </tr>
+    <tr class="1">
+        <td id="100">10.00</td>
+        <td id="101"></td>
+    </tr>
+    <tr class="2">
+        <td id="110">20.00</td>
+        <td id="111"></td>
+    </tr>
+    <tr class="2">
+        <td id="120">30.00</td>
+        <td id="121"></td>
+    </tr>
+    <tr class="2">
+        <td id="130">40.00</td>
+        <td id="131"></td>
+    </tr>
+    <tr class="2">
+        <td id="140">50.00</td>
+        <td id="141"></td>
+    </tr>
+    <tr class="2">
+        <td id="150">60.00</td>
+        <td id="151"></td>
+    </tr>
+    <tr class="2">
+        <td id="160">70.00</td>
+        <td id="161"></td>
+    </tr>
+    <tr class="2">
+        <td id="170">80.00</td>
+        <td id="171"></td>
+    </tr>
+    <tr class="2">
+        <td id="180">90.00</td>
+        <td id="181"></td>
+    </tr>
+    <tr class="2">
+        <td id="190">100.00</td>
+        <td id="191"></td>
+    </tr>
+    <tr class="3">
+        <td id="200">200.00</td>
+        <td id="201"></td>
+    </tr>
+    <tr class="3">
+        <td id="210">300.00</td>
+        <td id="211"></td>
+    </tr>
+    <tr class="3">
+        <td id="220">400.00</td>
+        <td id="221"></td>
+    </tr>
+    <tr class="3">
+        <td id="230">500.00</td>
+        <td id="231"></td>
+    </tr>
+    <tr class="3">
+        <td id="240">600.00</td>
+        <td id="241"></td>
+    </tr>
+    <tr class="3">
+        <td id="250">700.00</td>
+        <td id="251"></td>
+    </tr>
+    <tr class="3">
+        <td id="260">800.00</td>
+        <td id="261"></td>
+    </tr>
+    <tr class="3">
+        <td id="270">900.00</td>
+        <td id="271"></td>
+    </tr>
+    <tr class="3">
+        <td id="280">1000.00</td>
+        <td id="281"></td>
+    </tr>
+</table>
+    `
+
     document.getElementById('tableTitle').innerHTML = "Exchange rate "+currency2+"/"+currency1+" = "+rate.toFixed(4);
 
     document.getElementById('tableCurrency1').innerHTML = currency1;
@@ -42,6 +165,7 @@ function calculateTable(event) {
         let value = (Number(document.getElementById(i+'0').innerHTML)*rate).toFixed(2);
         document.getElementById(i+'1').innerHTML = String(value);
     }
+    (<HTMLInputElement>document.getElementById('exchangeRate')).value = String(rate.toFixed(2));
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
